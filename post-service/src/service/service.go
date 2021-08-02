414654bb3043d2da24528a58861d267fea91744c
Emoto13 <emcho_spassov@abv.bg>
2021-08-02 11:47:20 +0300

    fix missing username bug

--- post-service/src/service/service.go
+++ post-service/src/service/service.go
@@ -43,6 +43,7 @@ func (s *postService) CreatePost(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
+	post.Username = username
 	fmt.Println("Post recieved in CreatePost:", post)
 
 	err = s.postStore.CreatePost(username, post)
